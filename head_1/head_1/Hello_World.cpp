606d83d041eaf8065cb3f467bcc1436c782dec66
unknowoo7 <kazak1995kazak@yandex.ru>
2024-09-12 07:24:24 +0300

    Init repository

---
+++ head_1/head_1/Hello_World.cpp
@@ -0,0 +1,15 @@
+//#include <vector>;
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//	vector<string> v;
+//	ifstream in("Fillvector.cpp");
+//
+//
+//	return 0;
+//}
---
+++ head_1/head_1/head_1.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "There can by only one!!!\n";
+    return 0;
+}
